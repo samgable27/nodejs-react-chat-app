49fa32c5129ac7729f534cae4fbf9206cf86fce4
Sam <samarooniii@yahoo.com>
2023-06-27 16:41:00 -0400

    changed to advanced engine

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,19 +1,19 @@
-import { PrettyChatWindow } from "react-chat-engine-pretty";
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
 
 const ChatsPage = ({ user }) => {
-  //   const chatProps = useMultiChatLogic(
-  //   "0cee4fb6-f190-4eed-9516-2158254adb26",
-  //     user.username,
-  //     user.secret
-  //   );
+  const chatProps = useMultiChatLogic(
+    "0cee4fb6-f190-4eed-9516-2158254adb26",
+    user.username,
+    user.secret
+  );
   return (
     <div style={{ height: "100vh" }}>
-      <PrettyChatWindow
-        projectId="0cee4fb6-f190-4eed-9516-2158254adb26"
-        userName={user.username}
-        secret={user.secret}
-        style={{ height: "100%" }}
-      />
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
     </div>
   );
 };
